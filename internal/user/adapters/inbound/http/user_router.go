444e539f17b270acc1fcb38cc913d6c90460e9f0
Özgür Bayram <ozgurbayram@protonmail.com>
2024-09-15 00:13:21 +0300

    clean up

--- internal/user/adapters/inbound/http/user_router.go
+++
@@ -1,9 +0,0 @@
-package http
-
-import "github.com/gorilla/mux"
-
-func UserRouter(router *mux.Router, userController *UserController) {
-	r := router.PathPrefix("/user").Subrouter()
-
-	r.HandleFunc("", userController.CreateUser).Methods("GET")
-}
