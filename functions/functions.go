368266a90857963f52d2f495291b3df7395e82b0
Özgür Bayram <ozgurbayram@protonmail.com>
2024-09-14 17:09:29 +0300

    Create a simple flow

--- functions/functions.go
+++
@@ -1,6 +0,0 @@
-package functions
-
-func Fn() (int, int) {
-	total, value := 100, 100-10
-	return total, value
-}
--- main.go
+++ main.go
@@ -1,9 +1,4 @@
 package main
 
-import (
-	"mygogo/structs"
-)
-
 func main() {
-	structs.Structs()
 }
